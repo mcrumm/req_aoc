05c0c2a8a42ca74651e9ea0eb55ef08b2fdc0982
Michael Crumm <mike@crumm.net>
2022-12-02 23:29:40 -0800

    initial mix.new

---
+++ lib/req_aoc.ex
@@ -0,0 +1,18 @@
+defmodule ReqAOC do
+  @moduledoc """
+  Documentation for `ReqAOC`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReqAOC.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/req_aoc_test.exs
@@ -0,0 +1,8 @@
+defmodule ReqAOCTest do
+  use ExUnit.Case
+  doctest ReqAOC
+
+  test "greets the world" do
+    assert ReqAOC.hello() == :world
+  end
+end
